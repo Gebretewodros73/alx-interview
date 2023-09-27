6327a37e9816c650408281afb522167c06a7464e
gebretewodros73 <gebretewodros73@gmail.com>
2023-09-27 11:10:57 +0300

    Generates Pascals Triangle up to level n.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to level n.
+
+    Args:
+        n (int): The level up to which to generate Pascal's Triangle.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's Triangle.
+        None: If n is not a positive integer.
+    """
+    if not isinstance(n, int) or n <= 0:
+        return None
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j-1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
