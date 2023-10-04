a00b23c5871ca0d7820f2c10b953868826af0755
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-04 17:18:05 +0300

    try different aproch to lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,34 +9,23 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened.
 
     Args:
-        boxes (list of list): A list of lists
-        representing boxes and their keys.
+        boxes (list of list): A list of
+        lists representing boxes and their keys.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
+    def dfs(box_index):
+        opened_boxes.add(box_index)
+
+        for key in boxes[box_index]:
+            if key not in opened_boxes:
+                dfs(key)
+
     # Initialize a set to keep track of opened boxes
     opened_boxes = set()
 
-    # Add the first box to the set of opened boxes
-    opened_boxes.add(0)
+    # Start DFS from the first box
+    dfs(0)
 
-    # Initialize a set to keep track of keys we have
-    keys = set(range(len(boxes)))
-
-    # While we have keys and there are unopened boxes
-    while keys and opened_boxes:
-        # Get a box from the set of opened boxes
-        box = opened_boxes.pop()
-
-        # Add any new keys to the set of keys
-        keys.update(boxes[box])
-
-        # Remove the box from the set of keys
-        keys.remove(box)
-
-        # Add the new boxes to the set of opened boxes
-        opened_boxes.update(boxes[box])
-
-    # If we've opened all boxes, return True
     return len(opened_boxes) == len(boxes)
