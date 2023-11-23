d0416af2082f81cfb326a23c24884c656d77e957
gebretewodros73 <gebretewodros73@gmail.com>
2023-11-23 04:59:25 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""
-Change comes from within
-"""
+"""Change making module."""
+import sys
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values,
-    determine the fewest number
-    of coins needed to meet a given amount total.
-
-    Args:
-        coins (list): A list of the values of
-        the coins in your possession.
-        total (int): The target total amount.
-
-    Returns:
-        int: Fewest number of coins needed to meet the total.
-              If total is 0 or less, return 0.
-              If total cannot be met by any number of
-              coins you have, return -1.
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
     if total <= 0:
         return 0
 
-    # Initialize an array to store the
-    # minimum number of coins for each total value
-    dp = [float('inf')] * (total + 1)
+    # Initialize a table to store minimum coins needed for each amount
+    dp = [sys.maxsize] * (total + 1)
     dp[0] = 0
 
-    # Iterate through each coin value
+    # Update the table using each coin denomination
     for coin in coins:
-        # Update the minimum number of coins for each total value
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # If dp[total] is still infinity, the
-    # total cannot be met by any combination of coins
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != sys.maxsize else -1
