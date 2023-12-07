6ae872813d0e7cf849a403d527f77e5aa381e481
gebretewodros73 <gebretewodros73@gmail.com>
2023-12-07 03:07:39 +0300

    some correction

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
+"""Prime game module.
 """
-Prime Game module.
-"""
-
-
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
 
 
 def isWinner(x, nums):
-    """Determine the winner of the Prime Game."""
-    wins = {"Maria": 0, "Ben": 0}
-
-    for n in nums:
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        maria_turn = True
-
-        while primes:
-            chosen_prime = max(primes) if maria_turn else min(primes)
-            primes = [p for p in primes if p % chosen_prime != 0]
-            maria_turn = not maria_turn
-
-        if maria_turn:
-            wins["Ben"] += 1
-        else:
-            wins["Maria"] += 1
-
-    if wins["Maria"] > wins["Ben"]:
-        return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
-        return "Ben"
-    else:
+    """Determines the winner of a prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
         return None
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
